5d9311dd60e8a95d219f3c51a0b856a432cd96a9
Ayo3rd <ope.otegbade@gmail.com>
2020-07-07 21:34:41 -0700

    First Standard Comnit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+	// Define constants - constants cannot be changed after they have been declared.
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "root");
+	define("DB_NAME", "otegbade_calendar_db");
+?>
+
