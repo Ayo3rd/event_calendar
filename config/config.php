f26b00ab98c1f38f1492009e9a2fcc72b780092e
Ayo3rd <ope.otegbade@gmail.com>
2020-07-07 23:39:12 -0700

    Tryout again

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 	// Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "root");
-	define("DB_NAME", "otegbade_calendar_db");
+	define("DB_HOST", "db4free.net");
+	define("DB_USER", "realope");
+	define("DB_PASS", "january3rd");
+	define("DB_NAME", "calendar_db");
 ?>
 
