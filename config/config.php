1cbc6d41770f8ae1b75d25d75a826cfa21fa26bf
Ayo3rd <ope.otegbade@gmail.com>
2020-07-08 19:52:51 -0700

    Last One maybe

--- config/config.php
+++ config/config.php
@@ -1,8 +1,23 @@
 <?php
 	// Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "db4free.net");
-	define("DB_USER", "realope");
-	define("DB_PASS", "january3rd");
-	define("DB_NAME", "calendar_db");
+	// define("DB_HOST", "db4free.net");
+	// define("DB_USER", "realope");
+	// define("DB_PASS", "january3rd");
+	// define("DB_NAME", "calendar_db");
+
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+
+	define("DB_HOST", $cleardb_server);
+	define("DB_USER", $cleardb_username);
+	define("DB_PASS", $cleardb_password);
+	define("DB_NAME", $cleardb_db);
+
+
+
+
 ?>
 
