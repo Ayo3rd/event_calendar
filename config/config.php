1e253b1562019c30387c90b7d1d2ad1eccd162c1
Ayo3rd <ope.otegbade@gmail.com>
2020-07-08 22:24:50 -0700

    This was 9.00

--- config/config.php
+++ config/config.php
@@ -1,47 +1,47 @@
 <?php
-	//Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "db4free.net");
-	define("DB_USER", "realope");
-	define("DB_PASS", "january3rd");
-	define("DB_NAME", "calendar_db");
+	// //Define constants - constants cannot be changed after they have been declared.
+	// define("DB_HOST", "db4free.net");
+	// define("DB_USER", "realope");
+	// define("DB_PASS", "january3rd");
+	// define("DB_NAME", "calendar_db");
 
-// 	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// 	$cleardb_server = $cleardb_url["host"];
-// 	$cleardb_username = $cleardb_url["user"];
-// 	$cleardb_password = $cleardb_url["pass"];
-// 	$cleardb_db = substr($cleardb_url["path"],1);
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
 
-// 	define("DB_HOST", $cleardb_server);
-// 	define("DB_USER", $cleardb_username);
-// 	define("DB_PASS", $cleardb_password);
-// 	define("DB_NAME", $cleardb_db);
+	define("DB_HOST", $cleardb_server);
+	define("DB_USER", $cleardb_username);
+	define("DB_PASS", $cleardb_password);
+	define("DB_NAME", $cleardb_db);
 
 
 
-// 	$active_group = 'default';
-// $query_builder = TRUE;
+	$active_group = 'default';
+$query_builder = TRUE;
 
-// $db['default'] = array(
-//     'dsn'    => '',
-//     'hostname' => $cleardb_server,
-//     'username' => $cleardb_username,
-//     'password' => $cleardb_password,
-//     'database' => $cleardb_db,
-//     'dbdriver' => 'mysqli',
-//     'dbprefix' => '',
-//     'pconnect' => FALSE,
-//     'db_debug' => (ENVIRONMENT !== 'production'),
-//     'cache_on' => FALSE,
-//     'cachedir' => '',
-//     'char_set' => 'utf8',
-//     'dbcollat' => 'utf8_general_ci',
-//     'swap_pre' => '',
-//     'encrypt' => FALSE,
-//     'compress' => FALSE,
-//     'stricton' => FALSE,
-//     'failover' => array(),
-//     'save_queries' => TRUE
-// );
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 
 
 
