8e989462bfb889da3b3e92f8d31c72c3aa93bd7d
Ayo3rd <ope.otegbade@gmail.com>
2020-07-08 21:35:05 -0700

    This was 9.30

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	// Define constants - constants cannot be changed after they have been declared.
+	//Define constants - constants cannot be changed after they have been declared.
 	define("DB_HOST", "db4free.net");
 	define("DB_USER", "realope");
 	define("DB_PASS", "january3rd");
