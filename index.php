e482e1354beddfaf33748d9541e315ecfa613a52
Ayo3rd <ope.otegbade@gmail.com>
2020-07-08 23:30:16 -0700

    Final for Tonight

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php header("Location: home.php"); ?>
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<a href = "home.php">Click here</a>
+
+</body>
+</html>
+ -->
+
+
+
